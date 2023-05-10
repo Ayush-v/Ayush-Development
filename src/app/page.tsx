d68e63b4a06cb1ce288c40b425986a02cd937400
Ayush <62958067+Ayush-v@users.noreply.github.com>
2023-05-10 20:51:54 +0530

    working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="bg-square">
-      <div>
-        <h1>Hello World</h1>
-        <Link href="/links">Links</Link>
-      </div>
+      <h1>Hello World</h1>
+      <Link href="/links">Links</Link>
     </main>
   );
 }
